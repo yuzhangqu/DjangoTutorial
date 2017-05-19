a5f4584cc42060d157c0f8860f8128955a26e621
Yu Zhangqu <yuzhangqu@qq.com>
2017-05-19 10:54:37 +0800

    adjust wuhan timegap

--- flash/static/flash/js/main2.js
+++ flash/static/flash/js/main2.js
@@ -1,5 +1,5 @@
 var countStrs = ["③", "②", "①"];
-var timegap = [1, 0.8, 0.9, 1, 1.1];
+var timegap = [0, 0.6, 0.7, 0.9, 1];
 var mixStrs = ["纯加", "混合"];
 var headText = "闪电心算 -";
 var digitVal = 1; // 位数
