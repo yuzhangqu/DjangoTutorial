d7e298e7615dfade105f73de3efb6143ff51d186
yuzhangqu <yuzhangqu@qq.com>
2018-03-14 10:38:03 +0800

    适配2018年湖北省闪电心算规则

--- flash/static/flash/js/main.js
+++ flash/static/flash/js/main.js
@@ -45,3 +45,11 @@ function removetips(sz) {
     $(".card-text").text("");
     $(".card-text").css({ "font-size": sz + "vw" });
 }
+
+String.format = function(src){
+    if (arguments.length == 0) return null;
+    var args = Array.prototype.slice.call(arguments, 1);
+    return src.replace(/\{(\d+)\}/g, function(m, i){
+        return args[i];
+    });
+};
