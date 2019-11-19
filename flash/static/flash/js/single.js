cf1c8050b1d5c5add3748fd54ac116304a7bc70f
yuzhangqu <yuzhangqu@qq.com>
2019-11-19 13:46:29 +0800

    fix bug

--- flash/static/flash/js/single.js
+++ flash/static/flash/js/single.js
@@ -42,7 +42,7 @@ Single.prototype.generate = function() {
     }
 }
 
-var timegap = [0, 0.6, 0.7, 0.9, 1];
+var timegap = [0, 6, 7, 9, 10, 12];
 var headText = "直加直减闪电心算 -";
 var numVal = 10; // 总笔数
 var speed = timegap[1];  // 一笔0.几秒
